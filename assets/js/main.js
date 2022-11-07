5d84e391d5c40c409f0ddf2e8776a6aa570564ba
itomo8849 <tomolllllllo@gmail.com>
2022-11-07 21:17:44 +0900

    Without jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,42 @@
-$(function() {
-  $(window).on('scroll resize load', function() {
-    var setHeight = 80;
-    var wHeight = $(window).height();
-    var scrollTop = $(window).scrollTop();
-    $('.js_fadeInItem').each(function() {
-      var targetPosition = $(this).offset().top;
-      if(scrollTop > targetPosition - wHeight + setHeight) {
-        $(this).addClass('is_show');
-      }
-    });
-    $('.js_fadeInTrigger').each(function() {
-      var targetPosition = $(this).offset().top;
-      if(scrollTop > targetPosition - wHeight + setHeight) {
-        $(this).addClass('is_show');
-      }
-    });
+window.addEventListener("load", () => {
+  // boxをすべて取得
+  const boxSingle = document.querySelectorAll(".js_fadeInItem");
+  // boxをすべて取得
+  const boxParents = document.querySelectorAll(".js_fadeInTrigger");
+  // scrollイベントをセット
+  window.addEventListener("scroll", showBoxes);
+  // ロードのタイミングで一度発火
+  showBoxes();
 
-  });
+  function showBoxes() {
+    // 発火位置
+    const triggerBottom = window.innerHeight - 100;
+
+    boxSingle.forEach((box) => {
+      // boxの頭部分の座標を取得
+      const boxTop = box.getBoundingClientRect().top;
+
+      // boxの頭部分が発火位置を超えたら
+      if (boxTop < triggerBottom) {
+        box.classList.add("is_show");
+      } 
+      // クラスの脱着の場合
+      // else {
+      //   box.classList.remove("is_show");
+      // }
+    });
+    boxParents.forEach((box) => {
+      // boxの頭部分の座標を取得
+      const boxTop = box.getBoundingClientRect().top;
+
+      // boxの頭部分が発火位置を超えたら
+      if (boxTop < triggerBottom) {
+        box.classList.add("is_show");
+      } 
+      // クラスの脱着の場合
+      // else {
+      //   box.classList.remove("is_show");
+      // }
+    });
+  }
 });
